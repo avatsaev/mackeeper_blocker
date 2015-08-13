52e45e55ec4286b53fa763f4da75094b7b61f518
Bryce Davis <me@bryceadavis.com>
2015-08-13 16:27:10 -0400

    Button will write script to the users home dir

--- Mackeeper Blocker/ViewController.swift
+++ Mackeeper Blocker/ViewController.swift
@@ -10,7 +10,36 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    let file = ""
+    let file = "mackeeper_blocker.sh"
+    let contents = "#!/usr/bin/ruby\n\n" +
+    "#Author: Vatsaev Aslan (@avatsaev)\n" +
+    "#Repository: https://github.com/avatsaev/mackeeper_blocker\n" +
+    "#GUI Author: Bryce Davis (@mahimahi42)\n" +
+    "#GUI Repository: https://github.com/mahimahi42/mackeeper_blocker\n\n" +
+    "#request root previleges in order to modify hosts file\n" +
+    "if ENV[\"USER\"] != \"root\"\n" +
+    "\texec(\"sudo #{ENV['_']} #{ARGV.join(' ')}\")\n" +
+    "\texit\n" +
+    "end\n\n" +
+    "#initiate domain list array\n" +
+    "mk_address_list = [\n" +
+    "\"mackeeperapp.mackeeper.com\",\n" +
+    "\"mackeeperapp2.mackeeper.com\",\n" +
+    "\"mackeeperapp3.mackeeper.com\",\n" +
+    "\"mackeeper.com\"\n" +
+    "]\n\n" +
+    "#open hosts file in append mode\n" +
+    "open('/etc/hosts', 'a') do |f|\n" +
+    "\t#for every domain name\n" +
+    "\tmk_address_list.each do |addr|\n" +
+    "\t\tp \"Adding #{addr} to hosts file...\"\n\n" +
+    "\t\tf.puts \"127.0.0.1 #{addr}\"\n" +
+    "\t\tp \"✔︎ Done.\"\n" +
+    "\tend\n" +
+    "end\n\n" +
+    "p \"-----------\"\n" +
+    "p \"✔︎ Script finished running.\"\n\n" +
+    "exit"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +53,20 @@ class ViewController: NSViewController {
         }
     }
     
-    
+    func createScript() {
+        if let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+            let dir = dirs[0] //documents directory
+            let path = dir.stringByAppendingPathComponent(file);
+            
+            //writing
+            contents.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
+            
+            println("Wrote \(path)")
+        }
+    }
 
     @IBAction func blockMackeeper(sender: AnyObject) {
+        createScript()
     }
 
 }
