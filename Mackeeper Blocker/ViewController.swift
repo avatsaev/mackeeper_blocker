50a76a95eca88cf0f84f8a3a74a17be0599c5aa8
Bryce Davis <me@bryceadavis.com>
2015-08-13 16:05:58 -0400

    Initial Commit

---
+++ Mackeeper Blocker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mackeeper Blocker
+//
+//  Created by Bryce Davis on 8/13/15.
+//  Copyright (c) 2015 Bryce Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
