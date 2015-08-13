88f15ed45ce8ed7286936294694d629fa117e208
Bryce Davis <me@bryceadavis.com>
2015-08-13 17:40:32 -0400

    Add some helpful comments

--- Mackeeper Blocker/ViewController.swift
+++ Mackeeper Blocker/ViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    /// The path to the mackeeper_blocker.sh script
     var path:String?
     
     override func viewDidLoad() {
@@ -24,15 +25,32 @@ class ViewController: NSViewController {
         }
     }
     
-    func loadScript() {
+// MARK: Custom functions
+    
+    /**
+        loadScript() will grab the included mackeeper_blocker.sh script and return
+        the path as an Optional String.
+    
+        :returns: The path to the mackeeper_blocker script
+    */
+    func loadScript() -> String? {
         let bundle = NSBundle.mainBundle()
         path = bundle.pathForResource("mackeeper_blocker", ofType: "sh")
         
-        println(path!)
+        return path
     }
 
+    /**
+        blockMackeeper() first loads in the mackeeper_blocker script, then spawns
+        a new Terminal.app instance to run the script. Since the script updates
+        the /etc/hosts file (owned by root), the user must type in their password.
+    
+        This is a less-scary option than doing it all from the command line, to some.
+    
+        :param: sender the UIButton from the GUI
+    */
     @IBAction func blockMackeeper(sender: AnyObject) {
-        loadScript()
+        path = loadScript()
         
         if let pathStr = path {
             let task = NSTask()
